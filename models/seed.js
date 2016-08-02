6a088d621472cf3b24b594f4f0f0689758859313
Sprazzeus <sprazzeus@gmail.com>
2016-08-02 21:36:40 +0800

    commented out work on seed.js

--- models/seed.js
+++ models/seed.js
@@ -8,7 +8,7 @@ mongoose.connect('mongodb://default:default@ds019101.mlab.com:19101/spotlight');
 let education = new Education({
   name: 'LASALLE College of the Arts',
   date: '2010–2014',
-  description: 'Graduated from Design Communication, following the Foundation in Visual Studies programme.'
+  description: 'Graduated from Design Communication, following completion of screamthe Foundation in Visual Studies programme.'
 });
 
 education.save((err) => {
@@ -18,31 +18,31 @@ education.save((err) => {
   }
 });
 
-let work1 = new Work({
-  name: 'Nuisance Labs',
-  date: '2015–2016',
-  description: 'Co-founded multidisciplinary nano-studio, crafting creative services ranging from branding to video.'
-});
-
-work1.save((err) => {
-  if (err) console.log(err);
-  else {
-    console.log('Work Created ', work1);
-  }
-});
-
-let work2 = new Work({
-  name: 'Fitness Ration',
-  date: '2016',
-  description: 'Worked on redesigning brand and collateral, designing the architecture and UI for retail website, taxonomy for products, new product packaging, conducting UX interviews and research with existing users.'
-});
-
-work2.save((err) => {
-  if (err) console.log(err);
-  else {
-    console.log('Work Created ', work2);
-  }
-});
+// let work1 = new Work({
+//   name: 'Nuisance Labs',
+//   date: '2015–2016',
+//   description: 'Co-founded multidisciplinary nano-studio, crafting creative services ranging from branding to video.'
+// });
+//
+// work1.save((err) => {
+//   if (err) console.log(err);
+//   else {
+//     console.log('Work Created ', work1);
+//   }
+// });
+//
+// let work2 = new Work({
+//   name: 'Fitness Ration',
+//   date: '2016',
+//   description: 'Worked on redesigning brand and collateral, designing the architecture and UI for retail website, taxonomy for products, new product packaging, conducting UX interviews and research with existing users.'
+// });
+//
+// work2.save((err) => {
+//   if (err) console.log(err);
+//   else {
+//     console.log('Work Created ', work2);
+//   }
+// });
 
 let project1 = new Project({
   name: 'Mastermind',
