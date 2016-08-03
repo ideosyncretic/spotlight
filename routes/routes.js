3a644869c851e1ffb24e9de16561d986eff8aa80
Sprazzeus <sprazzeus@gmail.com>
2016-08-03 11:32:17 +0800

    removed source .env from package.json, added CORS to routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -7,6 +7,12 @@ const bodyParser = require('body-parser')
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({ extended: true }))
 
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('../views/index', { title: 'Sprazzeus' });
